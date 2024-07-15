1c8e169213e0c7a3138e215cb238b81df267bd81
surazz999 <surazsub999@gmail.com>
2024-07-15 10:23:15 +0545

    questions

---
+++ lib/answer_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.answerText, required this.ontap});
+  final String answerText;
+  final void Function() ontap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          )),
+      onPressed: ontap,
+      child: Text(answerText),
+    );
+  }
+}
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +11,25 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Question Screen'));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         const Text(
+            'The question',
+            style: TextStyle(color: Colors.white),
+          ),
+         const  SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'answer 1', ontap: (){},),
+          AnswerButton(answerText: 'answer 2', ontap: (){},),
+          AnswerButton(answerText: 'answer 3', ontap: (){},),
+          AnswerButton(answerText: 'answer 4', ontap: (){},),
+        
+        ],
+      ),
+    );
   }
 }
