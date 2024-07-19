e3f08954bb92257a38ccc2345d3ad8148f8ea1eb
surazz999 <surazsub999@gmail.com>
2024-07-19 09:34:31 +0545

    values saved

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
 
-
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i],
+        },
+      );
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
